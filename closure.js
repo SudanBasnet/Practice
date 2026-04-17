1d94452c9954fb96aa71c85139ed34efcdbb2222
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-17 11:40:41 +1000

    reusable functions

--- closure.js
+++ closure.js
@@ -64,3 +64,16 @@ const triple = multiplier(3);
 
 console.log(double(5)); // 10
 console.log(triple(5)); // 15
+
+//factory function is a reusable function
+function multiplier(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+
+const double = multiplier(2);
+const triple = multiplier(3);
+
+console.log(double(5)); // 10
+console.log(triple(5)); // 15
