78eee3c26ab8d28ad671d486d99e3d86e8b3ce30
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-17 11:26:30 +1000

    closure

---
+++ closure.js
@@ -0,0 +1,16 @@
+function outer() {
+  let count = 0;
+
+  function inner() {
+    count++;
+    console.log(count);
+  }
+
+  return inner;
+}
+
+const counter = outer();
+
+counter(); // 1
+counter(); // 2
+counter(); // 3
