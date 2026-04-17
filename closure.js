bcaa36d19057865366f6e3b6176390b5cc9ea3f4
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-17 11:36:08 +1000

    more examples

--- closure.js
+++ closure.js
@@ -14,3 +14,19 @@ const counter = outer();
 counter(); // 1
 counter(); // 2
 counter(); // 3
+
+const createCounter = () => {
+  let count = 0;
+
+  return () => {
+    count++;
+    return count;
+  };
+};
+
+const c1 = createCounter();
+console.log(c1()); // 1
+console.log(c1()); // 2
+
+const c2 = createCounter();
+console.log(c2()); // 1 (separate memory)
