a0a0426f09dadb9a9863c40d81d3308878746733
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-18 18:58:51 +1000

    hoisting var

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,7 @@
 console.log(a); // undefined
 var a = 10;
 // Hoisting = JavaScript moves declarations to the top of their scope before execution.
+var a; // hoisted
+console.log(a); // undefined
+a = 10;
+// var is hoisted and initialized with undefined
