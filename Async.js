c176e93197dfeac4e4a214cebcecc4ee88bd2d44
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-18 19:12:56 +1000

    synchronous

---
+++ Async.js
@@ -0,0 +1,12 @@
+console.log("Start");
+
+console.log("Task 1");
+console.log("Task 2");
+
+console.log("End");
+//Start
+// Task 1
+// Task 2
+// End
+
+// 👉 Simple, predictable, but can be slow/blocking
