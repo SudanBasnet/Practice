5bd39f67c6d7fb52f086685ab1ba9e3760577117
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-18 19:15:23 +1000

    async

--- Async.js
+++ Async.js
@@ -10,3 +10,55 @@ console.log("End");
 // End
 
 // 👉 Simple, predictable, but can be slow/blocking
+
+// 🔥 2. Asynchronous (Async)
+
+// 👉 Tasks can run in the background
+// 👉 JS does NOT wait → moves to next line
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Task 1");
+}, 2000);
+
+console.log("End");
+
+// ❌ Sync (bad for heavy tasks)
+const data = getDataFromServer(); // takes 5 sec
+console.log(data);
+
+// 👉 UI freezes 😓
+
+//good practice
+getDataFromServer().then((data) => {
+  console.log(data);
+});
+
+//🧠 5. Async Types in JS
+// setTimeout, setInterval
+// Promises (.then)
+// async/await
+// API calls (fetch)
+
+console.log("1");
+
+setTimeout(() => {
+  console.log("2");
+}, 0);
+
+console.log("3");
+
+// 1
+// 3
+// 2
+
+// 👉 Even 0ms delay runs later → because of event loop
+
+console.log("A");
+
+setTimeout(() => {
+  console.log("B");
+}, 1000);
+
+console.log("C");
