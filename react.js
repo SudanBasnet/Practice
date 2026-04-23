284af704fe834e6ba434eb9b3e9b8d4c416cd05d
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-23 23:31:04 +1000

    state

--- react.js
+++ react.js
@@ -8,3 +8,21 @@ const Profile = () => {
 };
 
 export default Profile;
+
+
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
